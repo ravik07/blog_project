1b968232ed9640ed832a49188a9985fec134e9f9
Ravi <kharteravi7@gmail.com>
2021-12-26 23:10:49 +0530

    [Entered url nav][about,contact,dashboard,login,logout]

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request,'blog/home.html')
+
+def about(request):
+	return render(request,'blog/about.html')
+
+def contact(request):
+	return render(request,'blog/contact.html')
+
+def dashboard(request):
+	return render(request,'blog/dashboard.html')
+
+def user_logout(request):
+	pass
+
+def signup(request):
+	return render(request,'blog/signup.html')
+
+def user_login(request):
+	return render(request,'blog/login.html')
--- miniblog/urls.py
+++ miniblog/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('about/', views.about, name = "about"),
+    path('contact/', views.contact, name = "contact"),
+    path('dashboard/', views.dashboard, name = "dashboard"),
+    path('logout/', views.user_logout, name = "logout"),
+    path('signup/', views.signup, name = "signup"),
+    path('login/', views.user_login, name = "login")
 ]
